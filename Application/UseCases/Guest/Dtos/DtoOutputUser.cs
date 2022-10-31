ce8211b5b720aece7aae84e509803ca3608406d2
Yohan <yohan.noel.huls@gmail.com>
2022-10-31 17:22:14 +0100

    Modify DtoOutputUser.cs

--- Application/UseCases/Guest/Dtos/DtoOutputUser.cs
+++ Application/UseCases/Guest/Dtos/DtoOutputUser.cs
@@ -2,5 +2,11 @@ namespace Application.UseCases.Guest.Dtos;
 
 public class DtoOutputUser
 {
+    public string Surname { get; set; }
     
+    public string Lastname { get; set; }
+    
+    public string Email { get; set; }
+
+    public int Age { get; set; }
 }
