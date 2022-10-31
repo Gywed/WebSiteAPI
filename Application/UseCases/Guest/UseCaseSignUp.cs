d7bd66737bfb41480906cadade0c4dfe81836169
Yohan <yohan.noel.huls@gmail.com>
2022-10-31 17:29:30 +0100

    Implement UseCaseSignUp

--- Application/UseCases/Guest/UseCaseSignUp.cs
+++ Application/UseCases/Guest/UseCaseSignUp.cs
@@ -1,6 +1,23 @@
+using Application.UseCases.Guest.Dtos;
+using Application.UseCases.Utils;
+using Infrastructure.Ef;
+
 namespace Application.UseCases.Guest;
 
-public class UseCaseSignUp
+public class UseCaseSignUp : IUseCaseWriter<DtoOutputUser,DtoInputCreateUser>
 {
-    
+    private readonly IUserRepository _userRepository;
+
+    public UseCaseSignUp(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+
+    public DtoOutputUser Execute(DtoInputCreateUser input)
+    {
+        var dbUser = _userRepository.Create(input.Surname, input.Lastname, input.Email, input.Age, input.Password);
+
+        return Mapper.GetInstance().Map<DtoOutputUser>(dbUser);
+    }
 }
