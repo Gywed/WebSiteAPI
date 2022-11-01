ff483198f40cd3a31ff306e62f3282eb39e39e4a
Yohan <yohan.noel.huls@gmail.com>
2022-11-01 14:42:43 +0100

    Add DtoInputLogUser

---
+++ Application/UseCases/Guest/Dtos/DtoInputLogUser.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.UseCases.Guest.Dtos;
+
+public class DtoInputLogUser
+{
+    [Required] public string Email { get; set; }
+    
+    [Required] public string Password { get; set; }
+    [Required] public string Role { get; set; }
+   
+}
