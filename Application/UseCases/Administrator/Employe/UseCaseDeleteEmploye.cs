b848bfde4dbdb064578d0dfe70f1ed83961bf4f0
adujacqu <alex.dujacquier@gmail.com>
2022-11-07 14:36:21 +0100

    Implement UseCaseDeleteEmploye.cs

--- Application/UseCases/Administrator/Employe/UseCaseDeleteEmploye.cs
+++ Application/UseCases/Administrator/Employe/UseCaseDeleteEmploye.cs
@@ -1,6 +1,20 @@
+using Application.UseCases.Utils;
+using Infrastructure.Ef;
+
+
 namespace Application.UseCases.Administrator.Employe;
 
-public class UseCaseDeleteEmploye
+public class UseCaseDeleteEmploye: IUseCaseParameterizedQuery<bool, int>
 {
-    
+    private IUserRepository _userRepository;
+
+    public UseCaseDeleteEmploye(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public bool Execute(int param)
+    {
+        return _userRepository.Delete(param);
+    }
 }
