d83e00f6277706f8ef0fd23fdb2a40e83097c70b
Yohan <yohan.noel.huls@gmail.com>
2022-11-13 15:17:32 +0100

    Remove Order attribut from OrderContent.cs

--- Domain/OrderContent.cs
+++ Domain/OrderContent.cs
@@ -3,9 +3,7 @@ namespace Domain;
 public class OrderContent
 {
     public int Id { get; set; }
-    
-    public Order Order { get; set; }
-    
+
     public Article Article { get; set; }
     
     public double Quantity { get; set; }
