a60db45c2080165f2854632757cdc3111b253369
adujacqu <alex.dujacquier@gmail.com>
2022-11-16 17:08:06 +0100

    Implement UseCaseFetchPaginationEmployee.cs with not required argument

--- Application/UseCases/Administrator/Employe/UseCaseFetchPaginationEmployee.cs
+++ Application/UseCases/Administrator/Employe/UseCaseFetchPaginationEmployee.cs
@@ -1,14 +1,28 @@
+using Application.UseCases.Administrator.Dtos;
 using Application.UseCases.Guest.Dtos;
 using Application.UseCases.Utils;
 using Infrastructure.Ef;
 
 namespace Application.UseCases.Administrator.Employe;
 
-public class UseCaseFetchPaginationEmployee:IUseCaseQuery<DtoOutputUser>
+public class UseCaseFetchPaginationEmployee:IUseCaseParameterizedQuery<IEnumerable<DtoOutputUser>, DtoInputPaginationFilteringParameters>
 {
     private IUserRepository _userRepository;
-    public DtoOutputUser Execute()
+
+    public UseCaseFetchPaginationEmployee(IUserRepository userRepository)
     {
-        return null;
+        _userRepository = userRepository;
+    }
+
+    public IEnumerable<DtoOutputUser> Execute(DtoInputPaginationFilteringParameters dto)
+    {
+        var nbPage = dto.nbPage ?? 1;
+        var nbElementsByPage = dto.nbElementsByPage ?? 10;
+        if (nbPage<1)
+        {
+            throw new ArgumentException($"nbPage must be above 0");
+        }
+        var dbUser = _userRepository.FetchPaginationEmployee(nbPage, nbElementsByPage);
+        return Mapper.GetInstance().Map<IEnumerable<DtoOutputUser>>(dbUser);
     }
 }
