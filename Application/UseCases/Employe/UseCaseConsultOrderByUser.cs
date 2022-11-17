06b76f76a3f261dbe48bd36e556b6adb7473ef4b
Yohan <yohan.noel.huls@gmail.com>
2022-11-17 11:26:49 +0100

    Implement UseCaseConsultOrderByUser

--- Application/UseCases/Employe/UseCaseConsultOrderByUser.cs
+++ Application/UseCases/Employe/UseCaseConsultOrderByUser.cs
@@ -1,6 +1,35 @@
+using Application.UseCases.Employe.Dtos;
+using Application.UseCases.Utils;
+using Infrastructure.Ef;
+using Microsoft.IdentityModel.Tokens;
+
 namespace Application.UseCases.Employe;
 
-public class UseCaseConsultOrderByUser
+public class UseCaseConsultOrderByUser : IUseCaseParameterizedQuery<IEnumerable<DtoOutputOrder>,string>
 {
-    
+    private readonly IOrderRepository _orderRepository;
+    private readonly UseCaseConsultOrderContent _useCaseConsultOrderContent;
+
+    public UseCaseConsultOrderByUser(IOrderRepository orderRepository, UseCaseConsultOrderContent useCaseConsultOrderContent)
+    {
+        _orderRepository = orderRepository;
+        _useCaseConsultOrderContent = useCaseConsultOrderContent;
+    }
+
+    public IEnumerable<DtoOutputOrder> Execute(string name)
+    {
+        var todayOrders = _orderRepository.FetchAllByUserName(name);
+
+        var dtos = new List<DtoOutputOrder>();
+
+        foreach (var dbOrder in todayOrders)
+        {
+            dtos?.Add(_useCaseConsultOrderContent.Execute(Mapper.GetInstance().Map<DtoInputOrder>(dbOrder)));
+        }
+
+        if (dtos.IsNullOrEmpty())
+            throw new ArgumentException($"There is no order for any user with the name {name}");
+        
+        return dtos;
+    }
 }
