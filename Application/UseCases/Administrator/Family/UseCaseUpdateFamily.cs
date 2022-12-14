e537d87f512b64993e9ed0a10fcfd432702b05a8
adujacqu <alex.dujacquier@gmail.com>
2022-12-14 18:07:49 +0100

    create UseCaseUpdateFamily

---
+++ Application/UseCases/Administrator/Family/UseCaseUpdateFamily.cs
@@ -0,0 +1,24 @@
+using Application.UseCases.Utils;
+using Infrastructure.Ef;
+using Infrastructure.Ef.DbEntities;
+
+namespace Application.UseCases.Administrator.Family;
+
+public class UseCaseUpdateFamily:IUseCaseWriter<bool, DtoInputUpdateFamily>
+{
+    private readonly IFamilyRepository _familyRepository;
+
+    public UseCaseUpdateFamily(IFamilyRepository familyRepository)
+    {
+        _familyRepository = familyRepository;
+    }
+
+    public bool Execute(DtoInputUpdateFamily dto)
+    {
+        return _familyRepository.Update(new DbFamily
+        {
+            id = dto.id,
+            family_name = dto.family_name
+        });
+    }
+}
