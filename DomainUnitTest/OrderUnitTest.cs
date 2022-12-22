701d24ef9a928d1fbcabd402389c08fc2ab1258d
adujacqu <alex.dujacquier@gmail.com>
2022-12-22 14:21:47 +0100

    add unit test for Of() in OrderUnitTest.cs

--- DomainUnitTest/OrderUnitTest.cs
+++ DomainUnitTest/OrderUnitTest.cs
@@ -8,15 +8,6 @@ public class OrderUnitTest
 {
 
     [Test]
-    public void Add_WithOneOrderContent_ReturnListSizeEqualOne()
-    {
-        var orderContent = new OrderContent();
-        var order = new Order();
-        order.Add(orderContent);
-        Assert.That(order.OrderContentItems().Count(), Is.EqualTo(1));
-    }
-    
-    [Test]
     public void TotalOrderPrice_WithSomeOrderContent_ReturnExpectedTotal()
     {
         var order = new Order();
@@ -40,6 +31,26 @@ public class OrderUnitTest
         order.Add(orderContent2);
         
         Assert.That(order.IsFullyPrepared(), Is.False);
+    }
+
+    [Test]
+    public void Of_WithEnumerableOfOrderContents_ReturnOrder()
+    {
+        var order = new Order();
+        var orderContent1 = OrderContentFactory.CreatePrice15Quantity2IsPreparedFalse();
+        var orderContent2 = OrderContentFactory.CreatePrice920Quantity1IsPreparedTrue();
+        
+        order.Add(orderContent1);
+        order.Add(orderContent2);
+
+        var orderContents = new List<OrderContent>().AsEnumerable();
+        orderContents = orderContents.Append(orderContent1);
+        orderContents = orderContents.Append(orderContent2);
+
+
+        var orderToTest = Order.Of(orderContents);
+        
+        Assert.That(orderToTest.OrderContentItems(), Is.EqualTo(order.OrderContentItems()));
     }
     
 }
