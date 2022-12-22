98f986415e10ece54c5e289f2fafc0c5bec3bdb6
adujacqu <alex.dujacquier@gmail.com>
2022-12-22 10:43:22 +0100

    add unit test for TotalOrderPrice() in OrderUnitTest.cs

--- DomainUnitTest/OrderUnitTest.cs
+++ DomainUnitTest/OrderUnitTest.cs
@@ -15,5 +15,20 @@ public class OrderUnitTest
         order.Add(orderContent);
         Assert.That(order.OrderContentItems().Count(), Is.EqualTo(1));
     }
+    
+    [Test]
+    public void TotalOrderPrice_WithSomeOrderContent_ReturnExpectedTotal()
+    {
+        var order = new Order();
+        var orderContent1 = OrderContentFactory.CreatePrice15Quantity2IsPreparedFalse();
+        var orderContent2 = OrderContentFactory.CreatePrice920Quantity1IsPreparedTrue();
+        
+        order.Add(orderContent1);
+        order.Add(orderContent2);
+        
+        Assert.That(order.TotalOrderPrice(), Is.EqualTo(39.2));
+    }
 
+    
+    
 }
