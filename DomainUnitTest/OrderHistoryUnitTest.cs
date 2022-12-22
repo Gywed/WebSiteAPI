1c40e15283460480c7e4d8440b8da6aacde5dc87
adujacqu <alex.dujacquier@gmail.com>
2022-12-22 14:29:05 +0100

    add unit test for Of() in OrderHistoryUnitTest.cs

--- DomainUnitTest/OrderHistoryUnitTest.cs
+++ DomainUnitTest/OrderHistoryUnitTest.cs
@@ -18,5 +18,25 @@ public class OrderHistoryUnitTest
         
         Assert.That(order.TotalOrderPrice(), Is.EqualTo(39.2));
     }
+    
+    [Test]
+    public void Of_WithEnumerableOfOrderHistoryContents_ReturnOrderHistory()
+    {
+        var order = new OrderHistory();
+        var orderContent1 = OrderHistoryContentFactory.CreatePrice15Quantity2();
+        var orderContent2 = OrderHistoryContentFactory.CreatePrice920Quantity1();
+        
+        order.Add(orderContent1);
+        order.Add(orderContent2);
+
+        var orderHistoryContents = new List<OrderHistoryContent>().AsEnumerable();
+        orderHistoryContents = orderHistoryContents.Append(orderContent1);
+        orderHistoryContents = orderHistoryContents.Append(orderContent2);
+
+
+        var orderToTest = OrderHistory.Of(orderHistoryContents);
+        
+        Assert.That(orderToTest.OrderHistoryContentItems(), Is.EqualTo(order.OrderHistoryContentItems()));
+    }
 
 }
