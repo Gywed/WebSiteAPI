96c27570c719615909ceb39eeb018c91dac25721
Yohan <yohan.noel.huls@gmail.com>
2022-12-23 12:37:44 +0100

    Modify UseCaseCreateArticle so it create a new path on it's own

--- Application/UseCases/Administrator/Article/UseCaseCreateArticle.cs
+++ Application/UseCases/Administrator/Article/UseCaseCreateArticle.cs
@@ -1,13 +1,11 @@
-using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Text;
+using System.Security.Cryptography;
 using Application.Services.Article;
 using Application.UseCases.Administrator.Article.Dtos;
 using Application.UseCases.Administrator.Dtos;
 using Application.UseCases.Utils;
 using Infrastructure.Ef;
-using Microsoft.IdentityModel.Tokens;
 
 namespace Application.UseCases.Administrator.Article;
 
@@ -15,11 +13,14 @@ public class UseCaseCreateArticle: IUseCaseWriter<DtoOutputArticle, DtoInputCrea
 {
     private readonly IArticleService _articleService;
     private readonly IArticleRepository _articleRepository;
+    private readonly UseCaseFetchAllArticleFileName _useCaseFetchAllArticleFileName;
 
-    public UseCaseCreateArticle(IArticleRepository articleRepository, IArticleService articleService)
+    public UseCaseCreateArticle(IArticleRepository articleRepository, IArticleService articleService
+        , UseCaseFetchAllArticleFileName useCaseFetchAllArticleFileName)
     {
         _articleRepository = articleRepository;
         _articleService = articleService;
+        _useCaseFetchAllArticleFileName = useCaseFetchAllArticleFileName;
     }
 
     public DtoOutputArticle Execute(DtoInputCreateArticle input)
@@ -27,6 +28,40 @@ public class UseCaseCreateArticle: IUseCaseWriter<DtoOutputArticle, DtoInputCrea
         // Give the place holder image if no path
         if (input.ImagePath == "")
             input.ImagePath = "assets/articles/No-Image-Placeholder.jpg";
+
+        // Give a new path if this is a new image
+        if (input.ImagePath == "new")
+        {
+            var filenames = _useCaseFetchAllArticleFileName.Execute();
+            var suitablePath = false;
+            var newPath = "";
+            while (!suitablePath)
+            {
+                var randomGenerator = RandomNumberGenerator.Create();
+                const int min = 501;
+                const int max = 9999999;
+
+                // Enough space to go up to 2147483647 (3 bytes who was 16777215 doesn't work for some reason)
+                var bytes = new byte[4];
+                randomGenerator.GetBytes(bytes);
+
+                // Convert To integer
+                var randomNumber = Math.Abs(BitConverter.ToInt32(bytes, 0));
+
+                // Check range
+                randomNumber = randomNumber % (max - min + 1) + min;
+                newPath = $"assets/articles/asset-{randomNumber}.jpg";
+                
+                // Check if the path already exist
+                suitablePath = true;
+                foreach (var name in filenames)
+                {
+                    if (newPath == name)
+                        suitablePath = false;
+                }
+            }
+            input.ImagePath = newPath;
+        }
         
         var dbArticle = _articleRepository.Create(input.Nametag, input.Price, input.Pricingtype
             , input.Stock, input.IdCategory, input.IdBrand, input.ImagePath);
